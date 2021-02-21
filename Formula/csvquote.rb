86511bd3d3bc43c295abec95eb6130fab8a7ee43
Scott S <scott@schlesier.ca>
2021-02-20 22:03:38 -0800

    fixup csvquote formula

--- Formula/csvquote.rb
+++ Formula/csvquote.rb
@@ -1,5 +1,5 @@
 class Csvquote < Formula
-  desc "Smart and simple CSV processing on the command line"
+  desc "Smart and simple CSV processing on the command-line"
   homepage "https://github.com/sschlesier/csvquote"
   url "https://github.com/sschlesier/csvquote/archive/v0.1.0.tar.gz"
   sha256 "71250f211031204f8aa5d6951a5cd6190b212617ce1e7c19aeac8fd61c70bd69"
@@ -8,7 +8,7 @@ class Csvquote < Formula
   def install
     system "make"
     ENV["PREFIX"]=ENV["HOMEBREW_FORMULA_PREFIX"] + "/bin"
-    system "mkdir", "-p", ENV["PREFIX"]
+    mkdir ENV["PREFIX"]
     system "make", "install"
   end
 
