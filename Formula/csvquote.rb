66b73df1660f290c2e87f588567cbf6ec8750b4d
Scott Schlesier <scott@schlesier.ca>
2021-12-14 00:30:29 -0800

    add checksum

--- Formula/csvquote.rb
+++ Formula/csvquote.rb
@@ -2,6 +2,7 @@ class Csvquote < Formula
   desc "Smart and simple CSV processing on the command-line"
   homepage "https://github.com/sschlesier/csvquote"
   url "https://github.com/sschlesier/csvquote/archive/v0.1.1.tar.gz"
+  sha256 "aac41eff847773b9f2c1d60e428b28de67b7036e6e52f26b270d6fd6546528cb"
   license "MIT"
 
   def install
